2f3de21520163a358dd5df6e48394c982937825e
Alexandr20-Kamenev77 <147322332+Alexandr20-Kamenev77@users.noreply.github.com>
2023-11-02 04:17:05 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <CityWeather />
+  </div>
+</template>
+<script>
+import CityWeather from "./components/Weather.vue";
+export default {
+  components: {
+    CityWeather,
+  },
+};
+</script>
